33c59112cb2563dd88fb19f6f55771eabeb698fa
mfakhru91 <mfakhrualfarizqi91@gmail.com>
2021-12-02 20:20:34 +0700

    update

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\Barang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends BaseController
@@ -42,18 +44,37 @@ class ProductController extends BaseController
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
-            'role' => 'required',
+            'category_id' => 'required',
+            'nama_produk' => 'required',
+            'path_gambar' => 'required|max:2000',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'stok' => 'required',
+            'barcode' => 'required',
         ]);
 
         if ($validator->fails()) {
-            return $this->sendError('Validation Error.', $validator->errors(),401);
+            return $this->sendError('Validation Error.', $validator->errors(), 401);
         }
 
-        // return $this->sendResponse($response,'Get Products Successfully',200);
+        $product = new Barang;
+        $product->user_id = Auth::id();
+        $product->category_id = $request->category_id;
+        $product->nama_produk = $request->nama_produk;
+        $product->harga_beli = $request->harga_beli;
+        $product->harga_jual = $request->harga_jual;
+        $product->stok = $request->stok;
+        $product->barcode = $request->barcode;
+
+        $getImage = $request->path_gambar;
+        $imageName = time() . '.' . $getImage->extension();
+        $imagePath = public_path() . '/images/product';
+        $product->path_gambar = $imageName;
+        $getImage->move($imagePath, $imageName);
+        $product->save();
+
+        $result['product'] = Barang::where('id', $product->id)->first();
+        return $this->sendResponse($result, 'Get Products Successfully', 200);
     }
 
     /**
@@ -64,7 +85,8 @@ class ProductController extends BaseController
      */
     public function show($id)
     {
-        //
+        $result['product'] = Barang::where('id', $id)->first();
+        return $this->sendResponse($result, 'Get Products Successfully', 200);
     }
 
     /**
@@ -87,7 +109,24 @@ class ProductController extends BaseController
      */
     public function update(Request $request, $id)
     {
-        //
+        $input = $request->all();
+        $product = Barang::find($id);
+        $getImage = $request->path_gambar;
+        $imageName = '';
+        if ($getImage) {
+            // $imageName = time().'.'.$getImage->extension();
+            // $imagePath = public_path(). '/images/product';
+            if (Storage::exists('public/images/product/1638194836.jpg')) {
+                // Storage::delete('image/product/'.$product->path_gambar);
+                $imageName = 'asdasdasddasd';
+            }
+            // $product['path_gambar'] = $imageName;
+            // $getImage->move($imagePath, $imageName);
+        }
+        https://github.com/shafwansyah/dev_kasir.git
+        // $product->fill($input)->save();
+        $result['product'] = Barang::where('id', $id)->first();
+        return $this->sendResponse($imageName, 'Get Products Successfully', 200);
     }
 
     /**
